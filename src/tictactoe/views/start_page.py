93671fa1c50a809683e26df25d0e04f8ff41f023
Chousspb <chous_spb@mail.ru>
2023-05-23 20:24:00 +0300

    index.html changes

--- src/tictactoe/views/start_page.py
+++ src/tictactoe/views/start_page.py
@@ -1,8 +1,16 @@
 from django.http import HttpRequest
 from django.shortcuts import render
-
+from tictactoe.models import Game
 
 def index(request: HttpRequest):
     if not request.COOKIES.get('game_id'):
         print('Can not find game_id cookie')
-    return render(request, 'tictactoe/templates/index.html')
+    title = "Список игр"
+    context = {"page_title": title}
+
+    games = Game.objects.all()
+    context['games'] = games
+    response = render(request, 'tictactoe/templates/index.html', context)
+
+
+    return response
